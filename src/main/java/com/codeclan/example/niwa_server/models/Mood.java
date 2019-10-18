2cfce3d65ee713f6494d2e39b1dff1513b1759fc
Kyle <kirvine87@gmail.com>
2019-10-18 16:53:04 +0100

    adds mood class

--- src/main/java/com/codeclan/example/niwa_server/models/Mood.java
+++ src/main/java/com/codeclan/example/niwa_server/models/Mood.java
@@ -1,4 +1,59 @@
 package com.codeclan.example.niwa_server.models;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "moods")
 public class Mood {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Date time;
+    private int rating;
+    private String tag;
+
+    public Mood() {
+    }
+
+    public Mood(Date time) {
+        this.time = time;
+        this.rating = 0;
+        this.tag = "";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 }
