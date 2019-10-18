52818f274fe67e6dfdd029d3d61d12ee88fbef56
Kyle <kirvine87@gmail.com>
2019-10-18 15:41:45 +0100

    adds initial commit

---
+++ src/main/java/com/codeclan/example/niwa_server/NiwaServerApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.niwa_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiwaServerApplication { 
+
+	public static void main(String[] args) {
+		SpringApplication.run(NiwaServerApplication.class, args);
+	}
+
+}
