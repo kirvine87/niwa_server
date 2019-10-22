bfa7337e3d6aac26c02c492c423c7ef35a60bc09
Stephen Blythe <stephenblythe@gmail.com>
2019-10-22 13:59:19 +0100

    Fixes issues with db

--- src/main/java/com/codeclan/example/niwa_server/repositories/days/DayRepository.java
+++ src/main/java/com/codeclan/example/niwa_server/repositories/days/DayRepository.java
@@ -3,8 +3,10 @@ package com.codeclan.example.niwa_server.repositories.days;
 import com.codeclan.example.niwa_server.models.Day;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
+@Transactional
 public interface DayRepository extends JpaRepository<Day, Long>, DayRepositoryCustom {
     List<Day> getDaysByIdBetween(Long id1, Long id2);
 
--- src/main/java/com/codeclan/example/niwa_server/repositories/days/DayRepositoryCustom.java
+++ src/main/java/com/codeclan/example/niwa_server/repositories/days/DayRepositoryCustom.java
@@ -1,9 +1,5 @@
 package com.codeclan.example.niwa_server.repositories.days;
 
-import com.codeclan.example.niwa_server.models.Day;
-
-import java.util.List;
-
 public interface DayRepositoryCustom {
 
 }
