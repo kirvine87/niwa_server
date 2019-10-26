30b36dfa534842476751c3ccab0e313ad27e8d36
Stephen Blythe <stephenblythe@gmail.com>
2019-10-26 11:38:57 +0100

    Adds /today custom route to /days, which crenders today's data if it exists already, and creates a new day row if it doesn't

--- src/main/java/com/codeclan/example/niwa_server/controller/DayController.java
+++ src/main/java/com/codeclan/example/niwa_server/controller/DayController.java
@@ -2,12 +2,14 @@ package com.codeclan.example.niwa_server.controller;
 
 import com.codeclan.example.niwa_server.models.Day;
 import com.codeclan.example.niwa_server.repositories.days.DayRepository;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -20,6 +22,18 @@ public class DayController {
     @GetMapping(value = "day/{id1}/day/{id2}")
     public List<Day> getWeeksMood(@PathVariable Long id1, @PathVariable Long id2){
         return dayRepository.getDaysByIdBetween(id1, id2);
+    }
+
+    @GetMapping(value = "today")
+    public List<Day> getToday(){
+        Date latest = dayRepository.findTopByOrderByIdDesc().get(0).getDate();
+        Date today = new Date();
+        if ( DateUtils.isSameDay(latest, today)) {
+            return dayRepository.findTopByOrderByIdDesc();
+        }
+        Day todayDay = new Day(today);
+        dayRepository.save(todayDay);
+        return dayRepository.findTopByOrderByIdDesc();
     }
 
     @GetMapping(value = "latest")
